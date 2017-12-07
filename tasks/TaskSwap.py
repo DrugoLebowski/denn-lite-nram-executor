7d654c79da5d9f72e3af9f5cd3f6df0ecd2054f4
Valerio Belli <belli.valerio@gmail.com>
2017-12-07 19:02:21 +0100

    Fix TaskSwap

--- tasks/TaskSwap.py
+++ tasks/TaskSwap.py
@@ -8,11 +8,14 @@ class TaskSwap(Task):
     """ Access the position in memory listed in the first position of the latter """
 
     def create(self) -> (np.array, np.array):
+        idx_1 = int(self.max_int / 3)
+        idx_2 = int(self.max_int / 2) + 1
         init_mem = np.random.randint(0, self.max_int, size=(self.batch_size, self.max_int), dtype=np.int32)
         init_mem[:, self.max_int - 1] = 0
+        init_mem[:, 0] = idx_1
+        init_mem[:, 1] = idx_2
 
         out_mem = init_mem.copy()
-        out_mem[:, int(self.max_int / 3)], out_mem[:, int(self.max_int / 2) + 1] = \
-            np.copy(out_mem[:, int(self.max_int / 2) + 1]), np.copy(out_mem[:, int(self.max_int / 3)])
+        out_mem[:, idx_1], out_mem[:, idx_2] = np.copy(out_mem[:, idx_2]), np.copy(out_mem[:, idx_1])
 
         return init_mem, out_mem
